cecad2cd09c7deff4a2a6816fcbc792a2a28210e
Yeoel <yorceseep@gmail.com>
2025-03-06 15:27:27 +0700

    feat: Add remove user from the collaborating

--- components/Document.tsx
+++ components/Document.tsx
@@ -10,6 +10,7 @@ import Editor from "./Editor";
 import useOwner from "@/lib/useOwner";
 import DeleteDocument from "./DeleteDocument";
 import InviteUser from "./InviteUser";
+import ManageUsers from "./ManageUsers";
 
 function Document({ id }: { id: string }) {
   const [data, loading, error] = useDocumentData(doc(db, "documents", id));
@@ -61,8 +62,8 @@ function Document({ id }: { id: string }) {
         </form>
       </div>
 
-      <div>
-        {/* Manage user */}
+      <div className="flex max-w-6xl mx-auto justify-between items-center mb-5">
+        <ManageUsers />
 
         {/* Avatars */}
       </div>
